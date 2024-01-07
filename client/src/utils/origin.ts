1646e9f2b98370ba23019b57c58ff91032af842e
yujunhui <lyujunhui@qq.com>
2024-01-07 18:27:33 +0800

    fix: use origin

--- client/src/utils/origin.ts
+++ client/src/utils/origin.ts
@@ -1,4 +1,8 @@
 const getUrls = () => {
+  if (process.env.NODE_ENV === 'production') {
+    return [window.location.origin];
+  }
+
   const baseUrl = 'http://' + (process.env.VUE_APP_API_IP || '127.0.0.1');
   const port = process.env.VUE_APP_API_PORT || 7001;
   const urls = [`${baseUrl}:${port}`];
